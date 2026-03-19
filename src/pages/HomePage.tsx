a2780205221672aa420d1ed3b1feb03868a83b04
BellasS <cpbellasmil@gmail.com>
2026-03-19 11:38:56 -0500

    Mejora de msje whatsapp

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -207,7 +207,7 @@ export default function HomePage() {
 
   const handleWhatsApp = () => {
     window.open(
-      "https://wa.me/51944830496?text=%C2%A1Hola!%20Quiero%20informaci%C3%B3n%20sobre%20sus%20servicios",
+      "https://wa.me/51944830496?text=%F0%9F%91%8B%20%C2%A1Hola!%20Estoy%20interesado%20en%20sus%20servicios%20tecnol%C3%B3gicos%20%F0%9F%92%BB%E2%9C%A8%0A%0A%C2%BFPodr%C3%ADan%20compartirme%20m%C3%A1s%20informaci%C3%B3n%20y%20asesorarme%3F%20%F0%9F%99%8C",
       "_blank"
     );
   };
